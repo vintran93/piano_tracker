e486e059d8a014ff7aaa6e445a73050d7d5a7902
vincent <vincentuong@yahoo.com>
2021-10-11 22:16:40 -0500

    application controller update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,4 +14,21 @@ class ApplicationController < Sinatra::Base
 		erb :index
 	end
 
+	helpers do
+
+		def link_to(url,text=url,opts={})
+			attributes = ""
+			opts.each { |key,value| attributes << key.to_s << "=\"" << value << "\" "}
+			"<a href=\"#{url}\" #{attributes}>#{text}</a>"
+  		end
+
+		def logged_in?
+			!!current_user
+		end
+	
+		def current_user
+		  @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+		end
+	end
+
 end
