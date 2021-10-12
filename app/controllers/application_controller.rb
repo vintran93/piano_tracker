ce853b843f5d3b63b98bb037eeadd81741706410
vincent <vincentuong@yahoo.com>
2021-10-11 21:33:34 -0500

    application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,17 @@
-require './config/environment'
+require "./config/environment"
+require "./app/models/user"
+
 
 class ApplicationController < Sinatra::Base
+	
+	configure do
+		set :views, "app/views"
+		enable :sessions
+		set :session_secret, "secret"
+	end
 
-  configure do
-    set :public_folder, 'public'
-    set :views, 'app/views'
-  end
-
-  get "/" do
-    erb :welcome
-  end
+	get "/" do
+		erb :index
+	end
 
 end
